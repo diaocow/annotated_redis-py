c1906767dbb58538ec52925c6d35be504806cc97
diaocow <diaocow@ubuntu.(none)>
2013-10-03 21:55:20 +0800

    修正注释缩进

--- redis/connection.py
+++ redis/connection.py
@@ -178,39 +178,39 @@ class HiredisParser(object):
         self._sock = None
         self._reader = None
 
-	# 解析类核心函数，
-	# 
-	# hiredis.Reader类提供了两个方法用来解析redis服务器返回的数据流，它们分别是feed和gets方法：
-	# 
-	# 1. feed(bytestream): 把bytestrem添加进内部缓存
-	# 2. gets(): 解析内部缓存中的数据并返回结果
-	# 
-	# 这两个方法非常简单，我们看一个例子即可明白：
-	# 
-	# >>> reader = hiredis.Reader()
-	# >>> reader.feed("$5\r\nhello\r\n")   # feed中参数为redis服务器返回的原始数据流
-	# >>> reader.gets()
-	# 'hello'
-	# 
-	# 若内部缓存中的数据不完整，gets()方法返回False
-	# 
-	# >>> reader.feed("*2\r\n$5\r\nhello\r\n")
-	# >>> reader.gets()
-	# False
-	# >>> reader.feed("$5\r\nworld\r\n") # 此时内部缓存中的数据为: *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
-	# >>> reader.gets()
-	# ['hello', 'world']
-	#
-	# 若内部缓存中含有多条回复，则需要多次调用gets()方法
-	#
-	# >>> reader.feed('*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n') # 譬如批量处理的命令回复
-	# >>> reader.gets()
-	# ['hello', 'world']
-	# >>> reader.gets()
-	# ['hello', 'world']
-	#
-	# 关于hiredis.Reader类更多信息请看:https://github.com/pietern/hiredis-py
-	#
+    # 解析类核心函数，
+    # 
+    # hiredis.Reader类提供了两个方法用来解析redis服务器返回的数据流，它们分别是feed和gets方法：
+    # 
+    # 1. feed(bytestream): 把bytestrem添加进内部缓存
+    # 2. gets(): 解析内部缓存中的数据并返回结果
+    # 
+    # 这两个方法非常简单，我们看一个例子即可明白：
+    # 
+    # >>> reader = hiredis.Reader()
+    # >>> reader.feed("$5\r\nhello\r\n")   # feed中参数为redis服务器返回的原始数据流
+    # >>> reader.gets()
+    # 'hello'
+    # 
+    # 若内部缓存中的数据不完整，gets()方法返回False
+    # 
+    # >>> reader.feed("*2\r\n$5\r\nhello\r\n")
+    # >>> reader.gets()
+    # False
+    # >>> reader.feed("$5\r\nworld\r\n") # 此时内部缓存中的数据为: *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+    # >>> reader.gets()
+    # ['hello', 'world']
+    #
+    # 若内部缓存中含有多条回复，则需要多次调用gets()方法
+    #
+    # >>> reader.feed('*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n') # 譬如批量处理的命令回复
+    # >>> reader.gets()
+    # ['hello', 'world']
+    # >>> reader.gets()
+    # ['hello', 'world']
+    #
+    # 关于hiredis.Reader类更多信息请看:https://github.com/pietern/hiredis-py
+    #
     def read_response(self):
         if not self._reader:
             raise ConnectionError("Socket closed on remote end")
@@ -225,8 +225,8 @@ class HiredisParser(object):
             if not buffer:
                 raise ConnectionError("Socket closed on remote end")
             self._reader.feed(buffer)
-			# 优化技巧：
-			# 若buffer中数据不是以'\n'结尾，则说明还未接受完服务器数据，直接开始下次循环(没必要调用gets()方法，因为肯定返回False）
+    		# 优化技巧：
+    		# 若buffer中数据不是以'\n'结尾，则说明还未接受完服务器数据，直接开始下次循环(没必要调用gets()方法，因为肯定返回False）
             if not buffer.endswith(SYM_LF):
                 continue
             response = self._reader.gets()
@@ -234,25 +234,25 @@ class HiredisParser(object):
 
 
 if HIREDIS_AVAILABLE:
-	# 如果安装了hiredis，则使用HiredisParser作为DefaultParser，
+    # 如果安装了hiredis，则使用HiredisParser作为DefaultParser，
     DefaultParser = HiredisParser
 else:
-	# 否则使用PythonParser作为DefaultParser
+    # 否则使用PythonParser作为DefaultParser
     DefaultParser = PythonParser
 
 
 # TCP连接实现类（默认连接实现类）
 class Connection(object):
-	
+    
     def __init__(self, host='localhost', port=6379, db=0, password=None,
                  socket_timeout=None, encoding='utf-8',
                  encoding_errors='strict', decode_responses=False,
                  parser_class=DefaultParser):
         self.pid = os.getpid()
-		# redis服务器的host/port
+    	# redis服务器的host/port
         self.host = host
         self.port = port
-		# 使用哪个数据库，默认使用编号为0的数据库（一个redis实例默认有16个数据库）
+    	# 使用哪个数据库，默认使用编号为0的数据库（一个redis实例默认有16个数据库）
         self.db = db
         self.password = password
         self.socket_timeout = socket_timeout
@@ -260,7 +260,7 @@ class Connection(object):
         self.encoding_errors = encoding_errors
         self.decode_responses = decode_responses
         self._sock = None
-		# redis response解析器，优先选择HiredisParser 
+    	# redis response解析器，优先选择HiredisParser 
         self._parser = parser_class()
 
     def __del__(self):
@@ -269,7 +269,7 @@ class Connection(object):
         except Exception:
             pass
 
-	# 连接Redis服务器
+    # 连接Redis服务器
     def connect(self):
         if self._sock:
             return
@@ -319,7 +319,7 @@ class Connection(object):
             return "Error %s connecting %s:%s. %s." % \
                 (exception.args[0], self.host, self.port, exception.args[1])
 
-	# 关闭与Redis服务器之间的连接
+    # 关闭与Redis服务器之间的连接
     def disconnect(self):
         self._parser.on_disconnect()
         if self._sock is None:
@@ -331,16 +331,16 @@ class Connection(object):
             pass
         self._sock = None
 
-	       raise
+           raise
 
-	# 向redis服务器发送命令
+    # 向redis服务器发送命令
     def send_command(self, *args):
         "Pack and send a command to the Redis server"
         self.send_packed_command(self.pack_command(*args))
 
-	# 把命令按照Redis协议转换，譬如： 
-	# set name diaocow =>
-	# *3\r\n$3\r\nset\r\n$4\r\nname\r\n$7\r\ndiaocow\r\n FIXME
+    # 把命令按照Redis协议转换，譬如： 
+    # set name diaocow =>
+    # *3\r\n$3\r\nset\r\n$4\r\nname\r\n$7\r\ndiaocow\r\n FIXME
     def pack_command(self, *args):
         "Pack a series of arguments into a value Redis command"
         args_output = SYM_EMPTY.join([
@@ -350,7 +350,7 @@ class Connection(object):
             (SYM_STAR, b(str(len(args))), SYM_CRLF, args_output))
         return output
 
-	# @see send_command
+    # @see send_command
     def send_packed_command(self, command):
         "Send an already packed command to the Redis server"
         if not self._sock:
@@ -369,7 +369,7 @@ class Connection(object):
         except:
             self.disconnect()
      
-	# 读取命令执行结果
+    # 读取命令执行结果
     def read_response(self):
         try:
             response = self._parser.read_response()
@@ -399,7 +399,7 @@ class UnixDomainSocketConnection(Connection):
                  socket_timeout=None, encoding='utf-8',
                  encoding_errors='strict', decode_responses=False,
                  parser_class=DefaultParser):
-		# 参数信息基本与Connection类相同，不再赘述
+    	# 参数信息基本与Connection类相同，不再赘述
         self.pid = os.getpid()
         self.path = path
         self.db = db
@@ -435,21 +435,21 @@ class ConnectionPool(object):
     def __init__(self, connection_class=Connection, max_connections=None,
                  **connection_kwargs):
         self.pid = os.getpid()
-		# 连接池中连接的实现类（默认为Connection类）
+    	# 连接池中连接的实现类（默认为Connection类）
         self.connection_class = connection_class
         self.connection_kwargs = connection_kwargs
-		# 连接池中允许的最大连接数
+    	# 连接池中允许的最大连接数
         self.max_connections = max_connections or 2 ** 31
-		# 目前连接池中已经创建的连接数
+    	# 目前连接池中已经创建的连接数
         self._created_connections = 0
-		# 连接池中的空闲连接
+    	# 连接池中的空闲连接
         self._available_connections = []
-		# 连接池中正在被使用的连接
+    	# 连接池中正在被使用的连接
         self._in_use_connections = set()
 
-	# 检测连接池有效性，不允许跨进程使用 
+    # 检测连接池有效性，不允许跨进程使用 
     def _checkpid(self):
-		# 若连接池所属进程不同于当前进程，则重新实例化连接池 
+    	# 若连接池所属进程不同于当前进程，则重新实例化连接池 
         if self.pid != os.getpid():
             self.disconnect()
             self.__init__(self.connection_class, self.max_connections,
@@ -461,28 +461,28 @@ class ConnectionPool(object):
         try:
             connection = self._available_connections.pop()
         except IndexError:
-			# 若没有可用的空闲连接，则创建一个新连接
+    		# 若没有可用的空闲连接，则创建一个新连接
             connection = self.make_connection()
         self._in_use_connections.add(connection)
         return connection
 
-	# 创建一个新连接
+    # 创建一个新连接
     def make_connection(self):
-		# 若当前连接池中的连接数已经超过max_connections，则raise exception
+    	# 若当前连接池中的连接数已经超过max_connections，则raise exception
         if self._created_connections >= self.max_connections:
             raise ConnectionError("Too many connections")
-		# ps: 其实这里不是线程安全的，存在race condition
-		self._created_connections += 1 
+    	# ps: 其实这里不是线程安全的，存在race condition
+    	self._created_connections += 1 
         return self.connection_class(**self.connection_kwargs)
 
-	# 释放一个连接（放回连接池）
+    # 释放一个连接（放回连接池）
     def release(self, connection):
         self._checkpid()
         if connection.pid == self.pid:
             self._in_use_connections.remove(connection)
             self._available_connections.append(connection)
-	
-	# 关闭连接池中的所有连接
+    
+    # 关闭连接池中的所有连接
     def disconnect(self):
         all_conns = chain(self._available_connections,
                           self._in_use_connections)
@@ -492,17 +492,17 @@ class ConnectionPool(object):
 
 # 另一个版本的连接池实现， 与redis-py 默认连接池ConnectionPool主要区别在于：
 # 当连接池中没有可用的空闲连接，并且此时连接数也已经到达了上限，若此时有客户端尝试获取一个连接，它不会抛出异常，而是根据timeout的值采取相应的措施：
-# 	a. timeout == None: 一直阻塞客户端，直到有一个连接可用
-# 	b. timeout == numsec: 在numsec时间范围内阻塞客户端，若还是没有可用连接，则抛出异常
+#     a. timeout == None: 一直阻塞客户端，直到有一个连接可用
+#     b. timeout == numsec: 在numsec时间范围内阻塞客户端，若还是没有可用连接，则抛出异常
 #
 class BlockingConnectionPool(object):
 
     def __init__(self, max_connections=50, timeout=20, connection_class=None,
                  queue_class=None, **connection_kwargs):
-		# 连接池中连接默认实现类：Connection
+    	# 连接池中连接默认实现类：Connection
         if connection_class is None:
             connection_class = Connection
-		# 连接池队列默认实现类：LifoQueue
+    	# 连接池队列默认实现类：LifoQueue
         if queue_class is None:
             queue_class = LifoQueue
 
@@ -513,7 +513,7 @@ class BlockingConnectionPool(object):
         self.timeout = timeout
         self.pid = os.getpid()
 
-		# 检测max_connections有效性
+    	# 检测max_connections有效性
         is_valid = isinstance(max_connections, int) and max_connections > 0
         if not is_valid:
             raise ValueError('``max_connections`` must be a positive integer')
@@ -528,22 +528,22 @@ class BlockingConnectionPool(object):
         # Keep a list of actual connection instances so that we can disconnect them later.
         self._connections = []
 
-	# 检测连接池有效性，不允许跨进程使用 
+    # 检测连接池有效性，不允许跨进程使用 
     def _checkpid(self):
         pid = os.getpid()
         if self.pid == pid:
             return
-		# 若连接池所属进程不同于当前进程，则重新实例化连接池 
+    	# 若连接池所属进程不同于当前进程，则重新实例化连接池 
         self.disconnect()
         self.reinstantiate()
 
-	# 创建一个新连接
+    # 创建一个新连接
     def make_connection(self):
         connection = self.connection_class(**self.connection_kwargs)
         self._connections.append(connection)
         return connection
 
-	# 获取一个可用连接
+    # 获取一个可用连接
     def get_connection(self, command_name, *keys, **options):
         """
         Get a connection, blocking for ``self.timeout`` until a connection
@@ -559,17 +559,17 @@ class BlockingConnectionPool(object):
         self._checkpid()
         connection = None
         try:
-			# 从连接池队列中获取一个连接
+    		# 从连接池队列中获取一个连接
             connection = self.pool.get(block=True, timeout=self.timeout)
         except Empty:
             raise ConnectionError("No connection available.")
 
-		# 若获取的connection对象为None，则创建一个新的连接
+    	# 若获取的connection对象为None，则创建一个新的连接
         if connection is None:
             connection = self.make_connection()
         return connection
 
-	# 释放连接（放回连接池）
+    # 释放连接（放回连接池）
     def release(self, connection):
         self._checkpid()
         try:
@@ -581,12 +581,12 @@ class BlockingConnectionPool(object):
             # want to reuse it.
             pass
 
-	# 关闭连接池中所有连接
+    # 关闭连接池中所有连接
     def disconnect(self):
         for connection in self._connections:
             connection.disconnect()
 
-	# 重新实例化连接池
+    # 重新实例化连接池
     def reinstantiate(self):
         self.__init__(max_connections=self.max_connections,
                       timeout=self.timeout,
